2b2d2d2f58f2207eee9a02d051113c9b92ade0fa
Ice Cream <falcon.with.hat@gmail.com>
2023-12-01 20:30:38 +0300

    All of methods is done

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import HE.MethodOne
 import KL.SecondMethod
 
 
-def write_In_File_Distr(Distr, N, name, n):
-    with open(name + '_' + str(n) + '_' + str(N) + '.dat', 'w') as file:
+def write_In_File_Distr(Distr, N, name, n, Hi):
+    with open("FILES\\"+ name + '_' + str(n) + '_' + str(N) + '_' + Hi + '.dat', 'w') as file:
         file.write(name + '(создано не ISW) N=' + str(N) + '\t n = ' + str(n) +  ' ' + '\n')
         file.write(str('0 ' + ' ' + str(N) + '\n'))
         for i in range(N):
@@ -16,47 +16,54 @@ def HenzeFunction(params, YObject, HEObject):
     n = params["n"]
     a = params["a"]
     N = params["N"]
+    typeHypothesis = params["typeHypothesis"]
     saverHE = []
 
     for i in range(N):
-        Y = YObject.ComputingY(n)
+        Y = YObject.ComputingY(n, typeHypothesis)
         HE = HEObject.MainHenze(Y, a, n)
         saverHE.append(HE)
         print(i)
-    write_In_File_Distr(saverHE, N, "Henze", n)
+    write_In_File_Distr(saverHE, N, "Henze", n, typeHypothesis)
+    saverHE.clear()
 
 def KlarFunction(params, YObject, KLObject):
     n = params["n"]
     a = params["a"]
     N = params["N"]
+    typeHypothesis = params["typeHypothesis"]
     saverKL = []
 
     for i in range(N):
-        Y = YObject.ComputingY(n)
+        Y = YObject.ComputingY(n, typeHypothesis)
         KL = KLObject.MainKlar(Y, a, n)
         saverKL.append(KL)
         print(i)
-    write_In_File_Distr(saverKL, N, "Klar", n)
+    write_In_File_Distr(saverKL, N, "Klar", n, typeHypothesis)
+    saverKL.clear()
 
 def BHFunction(params, YObject, BHObject):
     n = params["n"]
     a = params["a"]
     N = params["N"]
+    typeHypothesis = params["typeHypothesis"]
     saverBH = []
 
     for i in range(N):
-        Y = YObject.ComputingY(n)
+        Y = YObject.ComputingY(n, typeHypothesis)
         BH = BHObject.MainBaringhauseHenze(Y, a, n)
         saverBH.append(BH)
         print(i)
-    write_In_File_Distr(saverBH, N, "Baringhause_Henze", n)
+    write_In_File_Distr(saverBH, N, "Baringhause_Henze", n, typeHypothesis)
+    saverBH.clear()
 
 
 def Main():
-    n = 100
+    n = 10
     a = 1.
     N = 16600
-    params = {"n": n, "a": a, "N": N}
+    typeHypothesis = "H2"
+    params = {"n": n, "a": a, "N": N, "typeHypothesis": typeHypothesis}
 
     YObject = HE.GetY.CalculateY()
     HEObject = HE.MethodOne.Henze()
@@ -65,7 +72,7 @@ def Main():
 
     # HEMassive = HenzeFunction(params, YObject, HEObject)
     # KlarFunction(params, YObject, KLObject)
-    BHFunction(params, YObject, BHObject)
+    # BHFunction(params, YObject, BHObject)
 
 
     l = 0
